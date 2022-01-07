383caa0c15817a6d292fedeadf4f8939a09fe0ca
ZOrfeas <orfeas.zografos@gmail.com>
2022-01-07 15:50:50 +0200

    Add flink docker-compose file and flink-processor Java skeleton

---
+++ processor/src/main/java/com/bigdataclass/App.java
@@ -0,0 +1,13 @@
+package com.bigdataclass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
