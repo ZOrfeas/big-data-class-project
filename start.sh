b4e594b246451273e31ebee419b8efe41e882d49
GeorgePag4028 <georgepag4028@gmail.com>
2022-03-15 12:32:54 +0200

    fix test_producer

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ JM_CONTAINER=$(docker ps --filter name=jobmanager --format={{.ID}})
 docker exec $JM_CONTAINER flink run -d -c uni.processor.DataStreamJob /processor/processor.jar
 
 cd producer && \
-./producer.py
+./test_producer.py days=$1
