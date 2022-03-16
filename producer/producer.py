9a1b17291772677c140c7d346d24a7a575006089
ZOrfeas <orfeas.zografos@gmail.com>
2022-03-16 19:21:17 +0200

    Adjust producer.py to shoot a burst of data as if days have passed

--- producer/producer.py
+++ producer/producer.py
@@ -6,60 +6,92 @@ from datetime import datetime, timedelta
 import sys
 import random
 
-random.seed(100)
+# random.seed(100)
 
 id='producer_1'
 server='localhost:9092'
 topic='sensors'
-interval=1 # seconds
+interval=5 # seconds
 duration=15 # mins
+days=0
 
-def formatData(id, interval, duration, ):
-    sampleCount = int(duration*60/interval)
+def valueGen(filepath):
+    lines = open(filepath, 'r').readlines()
+    for line in lines:
+        if line.startswith("MinTemp"): continue
+        yield float(line.strip())
+
+def formatData(id, created_at, sampled_at, value):
+    return {
+        'id': id,
+        'created_at': created_at,
+        'sampled_at': sampled_at,
+        'value': value
+    }
+
+def dataRealTimeGen(id, interval, duration, valueGen):
+    sampleCount = int((duration*60)/interval)
     lateEventCount = int(sampleCount/30)
-    print(f'Generating {sampleCount} samples')
+    print(f'Generating {sampleCount} samples for realtime')
     counter=0
     for _ in range(sampleCount):
-        now = (datetime.now() - timedelta(days=1)).isoformat()
-        sampledAt = now
+        now = (datetime.now() - timedelta(days=2))
+        nowString = now.isoformat()
+        sampledAt = nowString
         if lateEventCount != 0 and (counter == 30 or counter+random.randint(1, 4) == 30):
             counter = 0
-            sampledAt = (datetime.strptime(now, '%Y-%m-%dT%H:%M:%S.%f') - timedelta(minutes=random.randint(10,20))).isoformat()
+            sampledAt = (now - timedelta(minutes=random.randint(10,20))).isoformat()
             lateEventCount -= 1
         counter += 1
-        yield {
-            'id': id,
-            'created_at': now,
-            'sampled_at': sampledAt,
-            'value': random.random()*10
-        }
+        datum = formatData(id, nowString, sampledAt, next(valueGen))
+        yield datum
 
+def dataBurstGen(id, interval, days, valueGen):
+    sampleCount = int(days*24*60*60/interval)
+    lateEventCount = int(sampleCount/30)
+    counter=0
+    now = datetime.now() - timedelta(days=days+2)
+    print(f'Generating {sampleCount} samples for burst')
+    for _ in range(sampleCount):
+        nowString = now.isoformat()
+        sampledAt = nowString
+        if lateEventCount != 0 and (counter == 30 or counter+random.randint(1, 4) == 30):
+            counter = 0
+            sampledAt = (now - timedelta(minutes=random.randint(10,20))).isoformat()
+            lateEventCount -= 1
+        counter += 1
+        datum = formatData(id, nowString, sampledAt, next(valueGen))
+        now = now + timedelta(seconds=interval)
+        yield datum
+    
 serializer = lambda v: json.dumps(v).encode('ascii')
 
-def main(id, server, topic, interval, duration):
+def main(id, server, topic, interval, duration, days):
     print(f'Creating producer {id} on {server}')
-    # producer = KafkaProducer(
-    #     client_id=f'{id}',
-    #     bootstrap_servers=[server],
-    #     value_serializer=serializer,
-    #     api_version=(0, 10, 1)
-    # )
+    producer = KafkaProducer(
+        client_id=f'{id}',
+        bootstrap_servers=[server],
+        value_serializer=serializer,
+        api_version=(0, 10, 1)
+    )
     print(f'Producer {id} created')
-    readyToSendData = formatData(id, interval, duration)
-    print(f'Sending samples')
-    for data in readyToSendData:
-        print(data)
-        break
+    valueGenInst = valueGen('temperature.csv')
+    burstData = dataBurstGen(id, interval, days, valueGenInst)
+    realTimeData = dataRealTimeGen(id, interval, duration, valueGenInst)
+    print(f'Sending burst of {days} days')
+    for data in burstData:
+        producer.send(topic, data)
 
-    # for data in readyToSendData:
-    #     producer.send(topic, data)
-    #     sleep(interval)
-    # print(f'Producer {id} finished')
+    print(f'Sending realtime samples')
+    for data in realTimeData:
+        producer.send(topic, data)
+        sleep(interval)
+    print(f'Producer {id} finished')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'help':
-        print(f'Usage: {sys.argv[0]} [id=<id>] [server=<server>] [topic=<topic>] [interval=<interval>] [duration=<duration>]')
-        print(f'Default values: id={id}, server={server}, topic={topic}, interval={interval}, duration={duration}')
+        print(f'Usage: {sys.argv[0]} [id=<id>] [server=<server>] [topic=<topic>] [interval=<interval>] [duration=<duration>] [days=<days>]')
+        print(f'Default values: id={id}, server={server}, topic={topic}, interval={interval}, duration={duration} days={days}')
         sys.exit(0)
     for arg in sys.argv[1:]:
         if arg.startswith('id='):
@@ -72,8 +104,9 @@ if __name__ == '__main__':
             interval = int(arg[9:])
         elif arg.startswith('duration='):
             duration = int(arg[9:])
+        elif arg.startswith('days='):
+            days = int(arg[5:])
         else:
             print(f'Unknown argument: {arg}')
             sys.exit(1)
-
-    main(id, server, topic, interval, duration)
+    main(id, server, topic, interval, duration, days)
