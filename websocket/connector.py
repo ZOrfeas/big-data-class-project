a3ef9a57281b138aa1e9833aaf9c8b6cbe1b0508
GeorgePag4028 <georgepag4028@gmail.com>
2022-03-27 19:21:04 +0300

    test

--- websocket/connector.py
+++ websocket/connector.py
@@ -9,7 +9,7 @@ token="eyJrIjoidmo1TGNmcUducmI5TVNDMHc4dUxaNUo2U2ZQQmdLMzEiLCJuIjoia2V5IiwiaWQiO
 token="Bearer " + token
 
 async def stream():
-    async with websockets.connect("ws://localhost:3000/api/live/push/sinewave_test", extra_headers={'Authorization': token}) as websocket:
+    async with websockets.connect("ws://localhost:3000/api/live/push/sinewave_test") as websocket:
         i = 0
         while True:
             await websocket.send("sin,val=" + str((math.sin(math.radians(i))+1)*20+80) +" " + str(time.time_ns()+1000000000))
