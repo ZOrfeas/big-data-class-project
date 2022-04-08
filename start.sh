de30d0ac16f94c5d74afb2ccac03166cf445658b
GeorgePag4028 <georgepag4028@gmail.com>
2022-04-08 16:35:27 +0300

    Multiple producers

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ JM_CONTAINER=$(docker ps --filter name=jobmanager --format={{.ID}})
 
 docker exec $JM_CONTAINER flink run -d -c uni.processor.DataStreamJob /processor/processor.jar
 
-cd producer && \
-./producer.py
+cd producer 
+./producer.py id='producer_1' &
+./producer.py id='producer_2' 
